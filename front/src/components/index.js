b827cf868114b45f0598a352348a7b730526d052
YarNik <yarnik75@gmail.com>
2020-05-22 17:49:47 +0300

    delete starting template(Hello)

--- front/src/components/index.js
+++ front/src/components/index.js
@@ -1,3 +1,1 @@
-export { default as HelloWorldOneFile } from './HelloWorldOneFile.vue';
-export { default as HelloWorld } from './hello-world/hello-world.component.vue';
 export { default as Logo } from './logo/logo.vue';
