f46765898743f31092816d9e0203b9d97dc781f8
Yaronberg <Yaronmen123@gmail.com>
2020-05-25 17:07:51 +0300

    fix path to image in base

--- server/Entities/Migrations/20200525122147_FixSeed.Designer.cs
+++ server/Entities/Migrations/20200525122147_FixSeed.Designer.cs
@@ -3,14 +3,16 @@ using Entities.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Entities.Migrations
 {
     [DbContext(typeof(FilmFinityDbContext))]
-    partial class FilmFinityDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200525122147_FixSeed")]
+    partial class FixSeed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
