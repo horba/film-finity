3615afcdabe0b50832e05cc6ef8f2d99ea63dc07
YarNik <yarnik75@gmail.com>
2020-05-27 21:42:54 +0300

    Update App.vue

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,8 +1,5 @@
 <template>
   <div id="app">
-    <div>
-      <router-link to="/">Home</router-link>
-    </div>
     <router-view />
   </div>
 </template>
@@ -20,4 +17,8 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+  body {
+    margin: 0px;
+  }
+</style>
