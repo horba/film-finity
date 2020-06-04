801c1f2f3ef8cf2d83431ed8a0a13e051b18ce99
YarNik <yarnik75@gmail.com>
2020-06-04 16:41:37 +0300

    Add 3 models

---
+++ server/Entities/Models/Actor.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class Actor
+    {
+        public int ActorId { get; set; }
+        public string ActorName { get; set; }
+
+        public ICollection<ActorsList> ActorsList { get; set; }
+    }
+}
---
+++ server/Entities/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class Movie
+    {
+        public int MovieId { get; set; }
+        public string MovieTitle { get; set; }        
+        public int CountStars { get; set; }
+        public int ReleaseYear { get; set; }
+        public string ImageSource { get; set; }
+
+        public ICollection<ActorsList> ActorsList { get; set; }
+
+    }
+}
