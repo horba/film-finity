2456b2a54b94193afeceee63f2cced715ea730ec
YarNik <yarnik75@gmail.com>
2020-06-04 16:46:43 +0300

    Add DTO

---
+++ server/WebAPI/DTO/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Models;
+
+namespace WebAPI.DTO
+{
+    public class MovieDTO
+    {
+        public int MovieId { get; set; }
+        public string MovieTitle { get; set; }
+        public int CountStars { get; set; }
+        public int ReleaseYear { get; set; }
+        public string ImageSource { get; set; }
+
+        public ICollection<ActorDTO> ActorsList { get; set; }
+    }
+}
