04694a7b0c72fd6013b2cf18b094256c40e41510
YarNik <yarnik75@gmail.com>
2020-06-04 16:37:15 +0300

    Update FilmFinityDbContext.cs
    described 3 new tables

--- server/Entities/DataAccess/FilmFinityDbContext.cs
+++ server/Entities/DataAccess/FilmFinityDbContext.cs
@@ -16,6 +16,10 @@ namespace Entities.DataAccess
         public DbSet<JobTitle> JobTitles { get; set; }
         public DbSet<Celebrity> Celebrities { get; set; }
         public DbSet<CelebrityJobTitles> CelebrityJobTitles { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<ActorsList> ActorsLists { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -31,6 +35,19 @@ namespace Entities.DataAccess
                 .HasOne(cj => cj.Celebrity)
                 .WithMany(j => j.CelebrityJobTitles)
                 .HasForeignKey(cjt => cjt.CelebrityId);
+
+            modelBuilder.Entity<ActorsList>()
+                .HasKey(cj => new { cj.MovieId, cj.ActorId });
+
+            modelBuilder.Entity<ActorsList>()
+                .HasOne(cj => cj.Actor)
+                .WithMany(j => j.ActorsList)
+                .HasForeignKey(cjt => cjt.ActorId);
+
+            modelBuilder.Entity<ActorsList>()
+                .HasOne(cj => cj.Movie)
+                .WithMany(j => j.ActorsList)
+                .HasForeignKey(cjt => cjt.MovieId);
 
             modelBuilder.Seed();
         }
