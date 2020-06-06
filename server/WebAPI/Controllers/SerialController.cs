e2d539e2dda84a62cf3cc65103276abff642cfb1
Whitetail11 <43177664+Whitetail11@users.noreply.github.com>
2020-06-06 19:54:59 +0300

    Add SerialController #16

---
+++ server/WebAPI/Controllers/SerialController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Entities.DataAccess;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.DTO;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SerialController : ControllerBase
+    {
+        private readonly FilmFinityDbContext dbContext;
+        public SerialController(FilmFinityDbContext filmFinityDbContext)
+        {
+            this.dbContext = filmFinityDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetListCelebrities()
+        {
+            List<SerialDTO> objectList = dbContext.Serials.Select(o => new SerialDTO
+            {
+                SerialId = o.SerialId,
+                Name = o.Name,
+                Rating = o.Rating,
+                Year = o.Year,
+                ImageSource = o.ImageSource,
+                GenreTitles = o.SerialGenreTitles.Select(j => j.GenreTitle).Select(j => new GenreDTO
+                { GenreId = j.GenreId, GenreName = j.GenreName }).ToList(),
+                Celebrities = o.SerialCelebrity.Select(j => j.Celebrity).Select(j => new CelebrityDTO
+                { 
+                    CelebrityId = j.CelebrityId,
+                    FirstName = j.FirstName,
+                    CountViews = j.CountViews,
+                    LastName = j.LastName,
+                    ImageSource = j.ImageSource
+                }).ToList()
+            }).ToList();
+            return Ok(objectList);
+        }
+    }
+}
