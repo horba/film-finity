3fafb1cf4d7bf7def026f8da63aebf9b81299487
Whitetail11 <43177664+Whitetail11@users.noreply.github.com>
2020-06-06 19:54:23 +0300

    Add DTO filex #16

---
+++ server/WebAPI/DTO/GenreDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.DTO
+{
+    public class GenreDTO
+    {
+        public int GenreId { get; set; }
+        public string GenreName { get; set; }
+    }
+}
---
+++ server/WebAPI/DTO/SerialDTO..cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.DTO
+{
+    public class SerialDTO
+    {
+        public int SerialId { get; set; }
+        public string Name { get; set; }
+        public double Rating { get; set; }
+        public int Year { get; set; }
+        public string ImageSource { get; set; }
+        public List<GenreDTO> GenreTitles { get; set; }
+        public List<CelebrityDTO> Celebrities { get; set; }
+    }
+}
