856a5b867717fba73ada0c9e84aaaf1492360a12
Yaronberg <Yaronmen123@gmail.com>
2020-06-07 15:51:25 +0300

    #10 add i18n library to project

--- front/src/main.js
+++ front/src/main.js
@@ -1,9 +1,11 @@
-import Vue from 'vue';
+import './element-variables.scss';
+
 import App from './App.vue';
+import ElementUI from 'element-ui';
+import Vue from 'vue';
+import i18n from 'vue-i18n';
 import router from '@router';
 import store from '@store';
-import ElementUI from 'element-ui';
-import './element-variables.scss';
 
 Vue.use(ElementUI);
 
@@ -12,5 +14,6 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
+  i18n,
   render: h => h(App)
 }).$mount('#app');
