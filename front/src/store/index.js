32aa940088fe198302fe089e5bc6c1c6db792fe5
YarNik <yarnik75@gmail.com>
2020-06-12 15:04:45 +0300

    Refactor(store) baseUrl to config.json #12
    add dev.config.json, transfer baseUrl value  to dev.config.json, import baseUrl  from dev.config.json #12

--- front/src/store/index.js
+++ front/src/store/index.js
@@ -2,18 +2,20 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import сelebrities from './modules/celebrities.store.js';
 import movies from './modules/movies.store.js';
+import * as data from '../../dev.config.json';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    baseUrl: 'https://localhost:44333'
+    baseUrl: data.baseUrl
   },
   mutations: {
   },
   actions: {
   },
   modules: {
-    сelebrities, movies
+    сelebrities,
+    movies
   }
 });
