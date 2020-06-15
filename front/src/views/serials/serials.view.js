796fd2b8212c113178a0189e3f30c8312912b1ca
Whitetail11 <43177664+Whitetail11@users.noreply.github.com>
2020-06-16 00:55:36 +0300

    update page #16

--- front/src/views/serials/serials.view.js
+++ front/src/views/serials/serials.view.js
@@ -12,15 +12,6 @@ export default {
   methods: {
     getImgSrc (imgPath) {
       return this.$store.state.baseUrl + '/' + imgPath;
-    },
-    actor (celebrity) {
-      let res = false;
-      for (let index = 0; index < celebrity.jobTitles.length; index++) {
-        if (celebrity.jobTitles[index].jobName === 'актер') {
-          res = true;
-        }
-      }
-      return res;
     }
   },
   filters: {
