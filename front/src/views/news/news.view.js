46c01d2105b1f87dc308dbe2fdef472a4aef96f5
volkovskiy4@gmail.com <volkovskiy4@gmail.com>
2020-06-20 00:05:28 +0300

    #15 add news example

--- front/src/views/news/news.view.js
+++ front/src/views/news/news.view.js
@@ -6,10 +6,13 @@ export default {
         {
           id: 1,
           title: 'Сиквел «Чудо-женщины» перенесли из-за коронавируса',
-          content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis ea veritati.',
+          content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis ea veritat'
+          + 'asasaasdasdadaisajs djams alsas dsad a dad adasasd asd adsada sadasd asdas dasd ad'
+          + 'asasaasdasdadaisajs djams alsas dsad a dad adasasd asd adsada sadasd asdas vlad ad',
           countViews: 233,
           previewImage: 'https://localhost:5001/StaticFiles/images/News/1.jfif',
-          publishTime: '2020-06-18T21:20:30.6683052',
+          publishTime: '18.06.2020 20:30',
+          countCommentaries: 5,
           author: {
             id: 1,
             firstName: 'Мария',
@@ -19,10 +22,19 @@ export default {
             {
               id: 1,
               name: 'Variety'
+            },
+            {
+              id: 2,
+              name: 'Film News'
             }
           ]
         }
       ]
     };
+  },
+  filters: {
+    modifyCategories (categories) {
+      return categories.map(element => ' ' + element.name).join();
+    }
   }
 };
