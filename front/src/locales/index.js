9f1f1c9f7d395a6ce81e03c48746305bcca72c1a
Yaronberg <Yaronmen123@gmail.com>
2020-06-21 23:30:23 +0300

    Add translations link to component #10

--- front/src/locales/index.js
+++ front/src/locales/index.js
@@ -1,6 +1,6 @@
-import en from './translations/en';
-import ru from './translations/ru';
-import ua from './translations/ua';
+import en from './translations/en.json';
+import ru from './translations/ru.json';
+import ua from './translations/ua.json';
 
 export default {
   en,
