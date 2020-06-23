07ad0be3086c3ff29162d57324168dc53a95d0c2
Whitetail11 <43177664+Whitetail11@users.noreply.github.com>
2020-06-23 16:21:17 +0300

    update serial service #29

--- server/WebAPI/Services/SerialsService.cs
+++ server/WebAPI/Services/SerialsService.cs
@@ -1,47 +1,28 @@
-﻿using Entities.DataAccess;
+﻿using AutoMapper;
+using Entities.DataAccess;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebAPI.DTO;
+using WebAPI.Repositories;
 
 namespace WebAPI.Services
 {
-    public interface ISerialsService
-    {
-        List<SerialDTO> GetSerials();
-    }
-
     public class SerialsService : ISerialsService
     {
-        private readonly FilmFinityDbContext dbContext;
-        public SerialsService(FilmFinityDbContext filmFinityDbContext)
+        private readonly ISerialRepository _serialRepository;
+        private readonly IMapper _mapper;
+
+        public SerialsService(ISerialRepository _serialRepository, IMapper _mapper)
         {
-            this.dbContext = filmFinityDbContext;
+            this._serialRepository = _serialRepository;
+            this._mapper = _mapper;
         }
         public List<SerialDTO> GetSerials()
         {
-            List<SerialDTO> objectList = dbContext.Serials.Select(o => new SerialDTO
-            {
-                Id = o.Id,
-                Name = o.Name,
-                Rating = o.Rating,
-                Year = o.Year,
-                PosterImageSource = o.PosterImageSource,
-                GenreTitles = o.SerialGenreTitles.Select(j => j.GenreTitle).Select(j => new GenreDTO
-                { Id = j.Id, Name = j.Name }).ToList(),
-                Celebrities = o.SerialCelebrity.Select(j => j.Celebrity).Select(j => new CelebrityDTO
-                {
-                    CelebrityId = j.CelebrityId,
-                    FirstName = j.FirstName,
-                    CountViews = j.CountViews,
-                    LastName = j.LastName,
-                    ImageSource = j.ImageSource,
-                    JobTitles = j.CelebrityJobTitles.Select(g => g.JobTitle).Select(g => new JobTitleDTO
-                    { JobTitleId = g.JobTitleId, JobName = g.JobName }).ToList()
-                }).ToList()
-            }).ToList();
-            return objectList;
+            var serials = _serialRepository.GetAllSerials();
+            return _mapper.Map<List<SerialDTO>>(serials.ToList());
         }
     }
 }
