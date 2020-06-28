33507c5fa5205b207e3af821c56986d5bbb10131
YarNik <yarnik75@gmail.com>
2020-06-28 17:23:02 +0300

    Resolve conflicts(server) #12
    delete migration, edit FilmFinityContext.cs, ModelBuilderExtensions.cs #12

--- server/Entities/DataAccess/FilmFinityDbContext.cs
+++ server/Entities/DataAccess/FilmFinityDbContext.cs
@@ -16,11 +16,7 @@ namespace Entities.DataAccess
         public DbSet<JobTitle> JobTitles { get; set; }
         public DbSet<Celebrity> Celebrities { get; set; }
         public DbSet<CelebrityJobTitles> CelebrityJobTitles { get; set; }
-        public DbSet<Movie> Movies { get; set; }
-        public DbSet<Actor> Actors { get; set; }
-        public DbSet<ActorsList> ActorsLists { get; set; }
-
-
+        
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<CelebrityJobTitles>()
@@ -35,20 +31,7 @@ namespace Entities.DataAccess
                 .HasOne(cj => cj.Celebrity)
                 .WithMany(j => j.CelebrityJobTitles)
                 .HasForeignKey(cjt => cjt.CelebrityId);
-
-            modelBuilder.Entity<ActorsList>()
-                .HasKey(cj => new { cj.MovieId, cj.ActorId });
-
-            modelBuilder.Entity<ActorsList>()
-                .HasOne(cj => cj.Actor)
-                .WithMany(j => j.ActorsList)
-                .HasForeignKey(cjt => cjt.ActorId);
-
-            modelBuilder.Entity<ActorsList>()
-                .HasOne(cj => cj.Movie)
-                .WithMany(j => j.ActorsList)
-                .HasForeignKey(cjt => cjt.MovieId);
-
+           
             modelBuilder.Seed();
         }
 
