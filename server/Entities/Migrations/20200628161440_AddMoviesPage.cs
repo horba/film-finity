9b3ed87a050b9c51d212d44abb04880302800e04
YarNik <yarnik75@gmail.com>
2020-06-28 19:32:01 +0300

    Refactor(server) add migration #12
    add migration after resolfing conflict, edit FilmFinityDbContext.cs and ModelBuilderExtensions.cs after conflict resolving  #12

---
+++ server/Entities/Migrations/20200628161440_AddMoviesPage.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Entities.Migrations
+{
+    public partial class AddMoviesPage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Actors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Actors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(nullable: true),
+                    Rate = table.Column<int>(nullable: false),
+                    ReleaseYear = table.Column<int>(nullable: false),
+                    ImageSource = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ActorsLists",
+                columns: table => new
+                {
+                    MovieId = table.Column<int>(nullable: false),
+                    ActorId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ActorsLists", x => new { x.MovieId, x.ActorId });
+                    table.ForeignKey(
+                        name: "FK_ActorsLists_Actors_ActorId",
+                        column: x => x.ActorId,
+                        principalTable: "Actors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ActorsLists_Movies_MovieId",
+                        column: x => x.MovieId,
+                        principalTable: "Movies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Actors",
+                columns: new[] { "Id", "FullName" },
+                values: new object[,]
+                {
+                    { 1, "Том Харпер" },
+                    { 25, "Джерни Смоллет-Белл" },
+                    { 26, "Мэри Элизабет Уинстэд" },
+                    { 27, "Грета Гервич" },
+                    { 29, "Тимоти Шаламе" },
+                    { 30, "Флоренс Пью" },
+                    { 31, "Эмма Уотсон" },
+                    { 32, "Сирзат Яхуп" },
+                    { 33, "Ху Цзюнь" },
+                    { 24, "Марго Робби" },
+                    { 34, "Юань Цуань" },
+                    { 36, "Аружан Джазильбекова" },
+                    { 37, "Фиби Фокс" },
+                    { 38, "Левин Ллойд" },
+                    { 39, "Бенедикт Камбербэтч" },
+                    { 40, "Ник Джонас" },
+                    { 41, "Николас Холт" },
+                    { 42, "Наташа Ротуэлл" },
+                    { 43, "Элайза Сканлен" },
+                    { 35, "Берик Айтжанов" },
+                    { 23, "Мэти Янь" },
+                    { 28, "Сирта Ронан" },
+                    { 21, "Тика Самптер" },
+                    { 2, "Филисити Джонс" },
+                    { 3, "Эдди Редмейн" },
+                    { 4, "Химет Патель" },
+                    { 5, "Режиссер" },
+                    { 22, "Бен Шварц" },
+                    { 7, "Дин-Чарльз Чакман" },
+                    { 8, "Ричард Мэдден" },
+                    { 9, "Джордж Нолфи" },
+                    { 10, "Энтони Маки" },
+                    { 11, "Сэмюел Лерой Джексон" },
+                    { 6, "Джордж Маккей" },
+                    { 13, "Джейк Кэздан" },
+                    { 14, "Дуэйн Джонсон" },
+                    { 15, "Карен Гиллан" },
+                    { 16, "Джейк Блэк" },
+                    { 17, "Кевин Харт" },
+                    { 18, "Джефф Фаулер" },
+                    { 19, "Джим Керри" },
+                    { 20, "Джеймс Марсден" },
+                    { 12, "Ниа Лонг" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "Id", "ImageSource", "Rate", "ReleaseYear", "Title" },
+                values: new object[,]
+                {
+                    { 8, "StaticFiles/images/18.jpg", 5, 2019, "Композитор" },
+                    { 7, "StaticFiles/images/17.jpg", 5, 2019, "Маленькие женщины" },
+                    { 6, "StaticFiles/images/16.jpg", 5, 2020, "Хищные птицы: Потрясающая история Харли Квинн" },
+                    { 5, "StaticFiles/images/15.jpg", 5, 2020, "Соник в кино" },
+                    { 2, "StaticFiles/images/12.jpg", 5, 2019, "1917" },
+                    { 3, "StaticFiles/images/13.jpg", 5, 2019, "Джуманджи: Новый уровень" },
+                    { 1, "StaticFiles/images/11.jpg", 5, 2019, "Аэронавты" },
+                    { 4, "StaticFiles/images/14.jpg", 5, 2020, "Банкир" }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "News",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "PublishTime",
+                value: new DateTime(2020, 6, 28, 19, 14, 39, 238, DateTimeKind.Local).AddTicks(7597));
+
+            migrationBuilder.UpdateData(
+                table: "News",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "PublishTime",
+                value: new DateTime(2020, 6, 28, 19, 14, 39, 247, DateTimeKind.Local).AddTicks(8229));
+
+            migrationBuilder.UpdateData(
+                table: "News",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "PublishTime",
+                value: new DateTime(2020, 6, 28, 19, 14, 39, 247, DateTimeKind.Local).AddTicks(8334));
+
+            migrationBuilder.UpdateData(
+                table: "News",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "PublishTime",
+                value: new DateTime(2020, 6, 28, 19, 14, 39, 247, DateTimeKind.Local).AddTicks(8346));
+
+            migrationBuilder.InsertData(
+                table: "ActorsLists",
+                columns: new[] { "MovieId", "ActorId" },
+                values: new object[,]
+                {
+                    { 1, 1 },
+                    { 5, 19 },
+                    { 5, 20 },
+                    { 5, 21 },
+                    { 5, 22 },
+                    { 5, 42 },
+                    { 6, 23 },
+                    { 6, 24 },
+                    { 6, 25 },
+                    { 5, 18 },
+                    { 6, 26 },
+                    { 7, 28 },
+                    { 7, 29 },
+                    { 7, 30 },
+                    { 7, 31 },
+                    { 7, 43 },
+                    { 8, 32 },
+                    { 8, 33 },
+                    { 8, 34 },
+                    { 7, 27 },
+                    { 8, 35 },
+                    { 4, 41 },
+                    { 4, 16 },
+                    { 1, 2 },
+                    { 1, 3 },
+                    { 1, 4 },
+                    { 1, 37 },
+                    { 1, 38 },
+                    { 2, 5 },
+                    { 2, 6 },
+                    { 2, 7 },
+                    { 4, 17 },
+                    { 2, 8 },
+                    { 3, 9 },
+                    { 3, 10 },
+                    { 3, 11 },
+                    { 3, 12 },
+                    { 3, 40 },
+                    { 4, 13 },
+                    { 4, 14 },
+                    { 4, 15 },
+                    { 2, 39 },
+                    { 8, 36 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ActorsLists_ActorId",
+                table: "ActorsLists",
+                column: "ActorId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ActorsLists");
+
+            migrationBuilder.DropTable(
+                name: "Actors");
+
+            migrationBuilder.DropTable(
+                name: "Movies");
+
+            migrationBuilder.UpdateData(
+                table: "News",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "PublishTime",
+                value: new DateTime(2020, 6, 27, 19, 48, 15, 285, DateTimeKind.Local).AddTicks(798));
+
+            migrationBuilder.UpdateData(
+                table: "News",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "PublishTime",
+                value: new DateTime(2020, 6, 27, 19, 48, 15, 287, DateTimeKind.Local).AddTicks(2699));
+
+            migrationBuilder.UpdateData(
+                table: "News",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "PublishTime",
+                value: new DateTime(2020, 6, 27, 19, 48, 15, 287, DateTimeKind.Local).AddTicks(2739));
+
+            migrationBuilder.UpdateData(
+                table: "News",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "PublishTime",
+                value: new DateTime(2020, 6, 27, 19, 48, 15, 287, DateTimeKind.Local).AddTicks(2744));
+        }
+    }
+}
--- server/Entities/ModelBuilderExtensions.cs
+++ server/Entities/ModelBuilderExtensions.cs
@@ -406,6 +406,179 @@ namespace Entities
                     NewsId = 4,
                     CategoryId = 1
                 });
+
+            modelBuilder.Entity<Actor>().HasData(
+               new Actor { Id = 1, FullName = "Том Харпер" },
+               new Actor { Id = 2, FullName = "Филисити Джонс" },
+               new Actor { Id = 3, FullName = "Эдди Редмейн" },
+               new Actor { Id = 4, FullName = "Химет Патель" },
+               new Actor { Id = 5, FullName = "Режиссер" },
+               new Actor { Id = 6, FullName = "Джордж Маккей" },
+               new Actor { Id = 7, FullName = "Дин-Чарльз Чакман" },
+               new Actor { Id = 8, FullName = "Ричард Мэдден" },
+               new Actor { Id = 9, FullName = "Джордж Нолфи" },
+               new Actor { Id = 10, FullName = "Энтони Маки" },
+               new Actor { Id = 11, FullName = "Сэмюел Лерой Джексон" },
+               new Actor { Id = 12, FullName = "Ниа Лонг" },
+               new Actor { Id = 13, FullName = "Джейк Кэздан" },
+               new Actor { Id = 14, FullName = "Дуэйн Джонсон" },
+               new Actor { Id = 15, FullName = "Карен Гиллан" },
+               new Actor { Id = 16, FullName = "Джейк Блэк" },
+               new Actor { Id = 17, FullName = "Кевин Харт" },
+               new Actor { Id = 18, FullName = "Джефф Фаулер" },
+               new Actor { Id = 19, FullName = "Джим Керри" },
+               new Actor { Id = 20, FullName = "Джеймс Марсден" },
+               new Actor { Id = 21, FullName = "Тика Самптер" },
+               new Actor { Id = 22, FullName = "Бен Шварц" },
+               new Actor { Id = 23, FullName = "Мэти Янь" },
+               new Actor { Id = 24, FullName = "Марго Робби" },
+               new Actor { Id = 25, FullName = "Джерни Смоллет-Белл" },
+               new Actor { Id = 26, FullName = "Мэри Элизабет Уинстэд" },
+               new Actor { Id = 27, FullName = "Грета Гервич" },
+               new Actor { Id = 28, FullName = "Сирта Ронан" },
+               new Actor { Id = 29, FullName = "Тимоти Шаламе" },
+               new Actor { Id = 30, FullName = "Флоренс Пью" },
+               new Actor { Id = 31, FullName = "Эмма Уотсон" },
+               new Actor { Id = 32, FullName = "Сирзат Яхуп" },
+               new Actor { Id = 33, FullName = "Ху Цзюнь" },
+               new Actor { Id = 34, FullName = "Юань Цуань" },
+               new Actor { Id = 35, FullName = "Берик Айтжанов" },
+               new Actor { Id = 36, FullName = "Аружан Джазильбекова" },
+               new Actor { Id = 37, FullName = "Фиби Фокс" },
+               new Actor { Id = 38, FullName = "Левин Ллойд" },
+               new Actor { Id = 39, FullName = "Бенедикт Камбербэтч" },
+               new Actor { Id = 40, FullName = "Ник Джонас" },
+               new Actor { Id = 41, FullName = "Николас Холт" },
+               new Actor { Id = 42, FullName = "Наташа Ротуэлл" },
+               new Actor { Id = 43, FullName = "Элайза Сканлен" }
+               );
+
+            modelBuilder.Entity<Movie>().HasData(
+
+                new Movie
+                {
+                    Id = 1,
+                    Title = "Аэронавты",
+                    Rate = 5,
+                    ReleaseYear = 2019,
+                    ImageSource = "StaticFiles/images/11.jpg"
+                },
+
+                new Movie
+                {
+                    Id = 2,
+                    Title = "1917",
+                    Rate = 5,
+                    ReleaseYear = 2019,
+                    ImageSource = "StaticFiles/images/12.jpg"
+                },
+
+                new Movie
+                {
+                    Id = 3,
+                    Title = "Джуманджи: Новый уровень",
+                    Rate = 5,
+                    ReleaseYear = 2019,
+                    ImageSource = "StaticFiles/images/13.jpg"
+                },
+
+                new Movie
+                {
+                    Id = 4,
+                    Title = "Банкир",
+                    Rate = 5,
+                    ReleaseYear = 2020,
+                    ImageSource = "StaticFiles/images/14.jpg"
+                },
+
+                new Movie
+                {
+                    Id = 5,
+                    Title = "Соник в кино",
+                    Rate = 5,
+                    ReleaseYear = 2020,
+                    ImageSource = "StaticFiles/images/15.jpg"
+                },
+
+                new Movie
+                {
+                    Id = 6,
+                    Title = "Хищные птицы: Потрясающая история Харли Квинн",
+                    Rate = 5,
+                    ReleaseYear = 2020,
+                    ImageSource = "StaticFiles/images/16.jpg"
+                },
+
+                new Movie
+                {
+                    Id = 7,
+                    Title = "Маленькие женщины",
+                    Rate = 5,
+                    ReleaseYear = 2019,
+                    ImageSource = "StaticFiles/images/17.jpg"
+                },
+
+                new Movie
+                {
+                    Id = 8,
+                    Title = "Композитор",
+                    Rate = 5,
+                    ReleaseYear = 2019,
+                    ImageSource = "StaticFiles/images/18.jpg"
+                });
+
+            modelBuilder.Entity<ActorsList>().HasData(
+                new ActorsList { MovieId = 1, ActorId = 1 },
+                new ActorsList { MovieId = 1, ActorId = 2 },
+                new ActorsList { MovieId = 1, ActorId = 3 },
+                new ActorsList { MovieId = 1, ActorId = 4 },
+                new ActorsList { MovieId = 1, ActorId = 37 },
+                new ActorsList { MovieId = 1, ActorId = 38 },
+
+                new ActorsList { MovieId = 2, ActorId = 5 },
+                new ActorsList { MovieId = 2, ActorId = 6 },
+                new ActorsList { MovieId = 2, ActorId = 7 },
+                new ActorsList { MovieId = 2, ActorId = 8 },
+                new ActorsList { MovieId = 2, ActorId = 39 },
+
+                new ActorsList { MovieId = 3, ActorId = 9 },
+                new ActorsList { MovieId = 3, ActorId = 10 },
+                new ActorsList { MovieId = 3, ActorId = 11 },
+                new ActorsList { MovieId = 3, ActorId = 12 },
+                new ActorsList { MovieId = 3, ActorId = 40 },
+
+                new ActorsList { MovieId = 4, ActorId = 13 },
+                new ActorsList { MovieId = 4, ActorId = 14 },
+                new ActorsList { MovieId = 4, ActorId = 15 },
+                new ActorsList { MovieId = 4, ActorId = 16 },
+                new ActorsList { MovieId = 4, ActorId = 17 },
+                new ActorsList { MovieId = 4, ActorId = 41 },
+
+                new ActorsList { MovieId = 5, ActorId = 18 },
+                new ActorsList { MovieId = 5, ActorId = 19 },
+                new ActorsList { MovieId = 5, ActorId = 20 },
+                new ActorsList { MovieId = 5, ActorId = 21 },
+                new ActorsList { MovieId = 5, ActorId = 22 },
+                new ActorsList { MovieId = 5, ActorId = 42 },
+
+                new ActorsList { MovieId = 6, ActorId = 23 },
+                new ActorsList { MovieId = 6, ActorId = 24 },
+                new ActorsList { MovieId = 6, ActorId = 25 },
+                new ActorsList { MovieId = 6, ActorId = 26 },
+
+                new ActorsList { MovieId = 7, ActorId = 27 },
+                new ActorsList { MovieId = 7, ActorId = 28 },
+                new ActorsList { MovieId = 7, ActorId = 29 },
+                new ActorsList { MovieId = 7, ActorId = 30 },
+                new ActorsList { MovieId = 7, ActorId = 31 },
+                new ActorsList { MovieId = 7, ActorId = 43 },
+
+                new ActorsList { MovieId = 8, ActorId = 32 },
+                new ActorsList { MovieId = 8, ActorId = 33 },
+                new ActorsList { MovieId = 8, ActorId = 34 },
+                new ActorsList { MovieId = 8, ActorId = 35 },
+                new ActorsList { MovieId = 8, ActorId = 36 }
+                );
         }
     }
 }
