457631c7ce1300410d68adf3ca283570d3a7e0b1
YarNik <yarnik75@gmail.com>
2020-06-30 21:29:40 +0300

    Refactor(store & variables) edit files due to conflicts #12
    edit store/index.js & variables.css due to conflicts #12

--- front/src/store/index.js
+++ front/src/store/index.js
@@ -4,6 +4,8 @@ import locale from './modules/locale.store.js';
 import сelebrities from './modules/celebrities.store.js';
 import news from './modules/news.store.js';
 import serials from './modules/serials.store.js';
+import movies from './modules/movies.store.js';
+import * as configurationData from '../../dev.config.json';
 
 Vue.use(Vuex);
 
@@ -19,6 +21,7 @@ export default new Vuex.Store({
     сelebrities,
     news,
     locale,
-    serials
+    serials,
+    movies
   }
 });
