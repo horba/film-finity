220452396f3f98c33ba87a135b7782592bcf2135
volkovskiy4@gmail.com <volkovskiy4@gmail.com>
2020-07-06 00:36:33 +0300

    #38 add final front

--- front/src/components/reviews/reviews-rows/reviews.component.js
+++ front/src/components/reviews/reviews-rows/reviews.component.js
@@ -1,16 +1,10 @@
 export default {
-  data: function () {
-    return {
-      rate: 4.0,
-      rateGeneral: 0.5
-    };
-  },
   props: {
     reviews: Array
   },
   methods: {
-    getImgSrc () {
-      return 'https://localhost:5001/StaticFiles/images/20.jpg';
+    getAverageFromArray (floats) {
+      return floats.reduce((a, b) => a + b) / floats.length;
     }
   },
   name: 'ff-review'
