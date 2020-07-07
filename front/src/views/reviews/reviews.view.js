6fa2b65177f6ea55ddb76366855134bacd01b9cd
volkovskiy4@gmail.com <volkovskiy4@gmail.com>
2020-07-07 19:11:09 +0300

    #38 add computed properties for el-rate

--- front/src/views/reviews/reviews.view.js
+++ front/src/views/reviews/reviews.view.js
@@ -8,18 +8,34 @@ export default {
   components: {
     FfReviewsBlocks, FfReviewsRows
   },
-  created () {
-    this.$store.dispatch('getReviews');
-    this.reviews = this.$store.state.reviews.reviews;
-  },
   computed: {
     getReviewsList () {
-      return this.$store.state.reviews.reviews;
+      return this.$store.state.reviews.reviews.data;
+    },
+    getTotalElements () {
+      return this.$store.state.reviews.reviews.totalCount;
+    },
+    getPageSize () {
+      return this.$store.state.reviews.reviews.pageSize;
+    },
+    getCurrentPageNumber () {
+      return this.$store.state.reviews.PageNumber;
     }
+  },
+  beforeCreate () {
+    this.$store.dispatch('getReviews', { PageSize: 2 });
   },
   methods: {
     onClick () {
       this.isRowView = !this.isRowView;
+    },
+    nextPageClicked () {
+      this.$store.commit('incrementCurrentPage');
+      this.$refs.reviews.pageChanged();
+    },
+    previousPageClicked () {
+      this.$store.commit('decrementCurrentPage');
+      this.$refs.reviews.pageChanged();
     }
   },
   name: 'ff-review'
